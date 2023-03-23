7bb98b7cea88a2d1d1987f37fc8f76f3e3fd8122
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-03-23 10:40:47 +0100

    Some adj

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ class Movie {
     public $year;
 
     // Definizione costruttore
-    public function __construct($title, $director, $year) {
-        $this->title = $title;
-        $this->director = $director;
-        $this->year = $year;
+    public function __construct(string $_title, string $_director, int $_year) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
     }
 
     // INZIO Definizione metodi getter
@@ -29,14 +29,17 @@ class Movie {
 
     // INIZIO Definizione metodi setter
     public function setTitle($title) {
+        if(!is_string($title) || $title === "") return false;
         $this->title = $title;
     }
 
     public function setDirector($director) {
+        if(!is_string($director) || $director === "") return false;
         $this->director = $director;
     }
 
     public function setYear($year) {
+        if(!is_numeric($year) || $year < 1800 || $year > date("Y")) return false;
         $this->year = $year;
     }
     // FINE Definizione metodi setter
@@ -50,4 +53,6 @@ $movie2 = new Movie("Inception", "Christopher Nolan", 2010);
 echo $movie1->getTitle() . " (" . $movie1->getYear() . ") - Directed by: " . $movie1->getDirector() . "<br>";
 echo $movie2->getTitle() . " (" . $movie2->getYear() . ") - Directed by: " . $movie2->getDirector();
 
+var_dump($movie1);
+var_dump($movie2);
 ?>
